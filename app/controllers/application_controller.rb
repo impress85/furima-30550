b1e3a096a1cf4c7cda567d44eae43ee05141c2ea
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-10-14 16:31:31 +0900

    追加カラムへストロングパラメータの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_parmitted_parameters, if: :devise_controller?
+
+
+  private
+  def configure_parmitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :first_name_katakana, :family_name, :family_name_katakana, :birthday])
+
 end
