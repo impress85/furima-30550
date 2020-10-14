b3cacadf1e1c327cb317bf50a439a64a3c644b34
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-10-14 18:08:50 +0900

    新規登録 必須項目の入力制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :first_name_katakana, presence: true
+  validates :family_name, presence: true
+  validates :family_name_katakana, presence: true
+  validates :birthday, presence: true
+
 end
