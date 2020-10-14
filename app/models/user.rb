38ef2198b4fdfe1e5635f02e68f57cebde3e1de4
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-10-14 21:30:49 +0900

    新規登録時の入力制限（パスワードを除く）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :first_name_katakana, presence: true
-  validates :family_name, presence: true
-  validates :family_name_katakana, presence: true
+  validates :first_name,
+    format:{ with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
+  validates :first_name_katakana,
+    format:{ with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :family_name,
+    format:{ with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
+  validates :family_name_katakana,
+    format:{ with: /\A[ァ-ヶー－]+\z/ }, presence: true
   validates :birthday, presence: true
 
 end
