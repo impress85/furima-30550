f5882e7519ac66ed744a64683b2886584e4b4bc7
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-10-16 00:17:10 +0900

    テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,9 +74,15 @@ describe User do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
-      it "passwordが半角英数字混合でなければ登録できない" do
+      it "passwordが半角数字だけでは登録できない" do
         @user.password = "123456"
         @user.password_confirmation = "123456"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it "passwordが半角英字だけでは登録できない" do
+        @user.password = "abcdef"
+        @user.password_confirmation = "abcdef"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid")
       end
@@ -97,9 +103,14 @@ describe User do
         expect(@user.errors.full_messages).to include("First name is invalid")
       end
       it "first_name_katakanaが空だと登録できない" do
-        @user.first_name = nil
+        @user.first_name_katakana = nil
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank")
+        expect(@user.errors.full_messages).to include("First name katakana can't be blank")
+      end
+      it "first_name_katakanaが全角カナ以外だと登録できない" do
+        @user.first_name_katakana = "たろう"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name katakana is invalid")
       end
       it "family_nameが空だと登録できない" do
         @user.family_name = nil
@@ -110,6 +121,11 @@ describe User do
         @user.family_name_katakana = nil
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name katakana can't be blank")
+      end
+      it "family_name_katakanaが全角カタカナ以外だと登録できない" do
+        @user.family_name_katakana = "さとう"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name katakana is invalid")
       end
       it "birthdayが空だと登録できない" do
         @user.birthday = nil
