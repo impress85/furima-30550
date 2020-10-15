0d9f20d520f130f083fc3711fcbaedacf73fdfe1
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-10-15 23:43:09 +0900

    FactoryBotのパスワード変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
       nickname {Gimei.name}
       email {Faker::Internet.free_email}
-      password = Faker::Internet.password(min_length: 6)
+      password = "123aaa"
 	    password {password}
       password_confirmation {password}
       first_name { Gimei.name.first }
