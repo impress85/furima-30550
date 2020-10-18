7d1dbabb1e8b2b9146a7242033d87b6dd703e21a
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-10-18 17:58:50 +0900

    Itemモデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name { "名前のテスト"}
+    price { "999999" }
+    describe { "説明のテスト" }
+    user_id { "1" }
+    category_id {"2"}
+    condition_id {"2"}
+    shipping_cost_payer_id {"2"}
+    prefectures_id {"2"}
+    shipping_days_id {"2"}
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
