672f73c498919eb1766eb991a2560dc8865714aa
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-10-19 21:34:23 +0900

    updateアクションの条件分岐の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    if  @item.valid?
+    if @item.update(item_params)
        redirect_to root_path
     else
        render :edit
